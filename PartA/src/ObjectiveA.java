dfb575651be5d0fe88e57f8062de22a4c912c8ec
Ethan Greis <GreisEH19@pwcs-edu.org>
2019-01-08 10:14:51 -0500

    The Eclipse Project Person A

---
+++ PartA/src/ObjectiveA.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ObjectiveA {
+	
+
+	public static void main(String[] args) 
+	{
+		System.out.println( calculateAvrage() );
+	}
+	
+	public static double calculateAverage()
+	{
+		System.out.println( "Please enter two integers to be averaged." );
+		Scanner scan = new Scanner(System.in);
+		int val1 = scan.nextInt();
+		int val2 = scan.nextInt();
+		return val1 + val2 / 2.o;
+	}
+
+}
